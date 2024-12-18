e47ffd1dd108f0377a7677b8a4178cb5bd6dbced
FrancescoCialfiDev <cialfifrancesco02@gmail.com>
2024-12-18 20:43:14 +0100

    capturing file url

--- src/components/MainComponent/MainComponent.jsx
+++ src/components/MainComponent/MainComponent.jsx
@@ -27,51 +27,57 @@ export const MainComponent = ({ data }) => {
     })
 
     // State of variables - Stato reattivo delle variabili del form.
-    const [title, setTitle] = useState("")
-    const [image, setImage] = useState("")
-    const [content, setContent] = useState("")
+    const [title, setTitle] = useState("");
+    const [image, setImage] = useState("");
+    const [content, setContent] = useState("");
 
 
 
     // Submit function - Funzione che si attiva all'invio del form
     const submitFunction = (event) => {
-        event.preventDefault() // Previene il comportamento default di refresh del form
-        alert("The card has been generated") // Mostriamo un alert su schermo se il processo è andato a buon fine.
+        event.preventDefault(); // Previene il comportamento default di refresh del form
+        alert("The card has been generated"); // Mostriamo un alert su schermo se il processo è andato a buon fine.
 
         // Inizializziamo una costante come oggetto con rispettive chiavi - valore dei nuovi elementi creati tramite form
         const newCardModel = {
 
             id: 1,
             title: title,
-            image: image,
+            image: image ? URL.createObjectURL(image) : "",
             content: content,
             published: true,
 
         }
 
         // In react i cambiamenti interni ad una variabile non vengono aggiornati in automatico, bisogna utilizzare l'intera reference della costante che racchiude l'array di oggetti, faro uno spread degli elementi contenuti al suo interno piu quelli nuovi generati e creare un nuovo array. Cosi facendo react rileva una modifica è mostrerà i cambiamenti effettivi.
-        const newCard = [...reactData, newCardModel]
-        setReactData(newCard) // Funzione che permette di cambiare il valore di reactData con il nuovo array di oggetti creato.
+        const newCard = [...reactData, newCardModel];
+        setReactData(newCard); // Funzione che permette di cambiare il valore di reactData con il nuovo array di oggetti creato.
 
 
     }
 
     //Remove Click Function
     function removeClick() {
-        const updateData = reactData.filter(element => element.id != event.target.closest(".card").id)
-        setReactData(updateData)
+        // Verifichiamo che l'id dell'elemento cliccato sia diverso da quello presente negli elementi dell'array
+        // Cosi da creare un array con solo all'interno gli oggetti con id diverso da quello cliccato, cosi da eliminare quelli con id uguale.
+        const updateData = reactData.filter(element => element.id != event.target.closest(".card").id);
+        setReactData(updateData); // Aggiorniamo lo stato della variabile.
+
     }
 
-    // Function for change Input
+    // Function for change Input - Modifichiamo il valore della nuova card creata con i valori inseriti nel form.
     const changeTitle = (event) => {
-        setTitle(event.target.value)
+        setTitle(event.target.value);
     }
     const selectImage = (event) => {
-        setImage(event.target.value)
+        const file = event.target.files[0]
+        setImage(file)
     }
     const changeContent = (event) => {
-        setContent(event.target.value)
+        setContent(event.target.value);
     }
+
+
 
 
 
@@ -85,7 +91,7 @@ export const MainComponent = ({ data }) => {
                         <label htmlFor="titleForm"><span className="fw-bold">Card&apos;s Name</span></label>
                         <input type="text" value={title} className="form-control" id="titleForm" placeholder="Es: React Components" onChange={changeTitle} />
                         <label htmlFor="imageForm"><span className="fw-bold">Card&apos;s Images</span></label>
-                        <input type="file" value={image} className="form-control" id="imageForm" onChange={selectImage} />
+                        <input type="file" className="form-control" id="imageForm" onChange={selectImage} />
                         <label htmlFor="contentForm"><span className="fw-bold">Card&apos;s Content</span></label>
                         <input type="text" value={content} className="form-control" id="contentForm" placeholder="Es: Text of the content" onChange={changeContent} />
                     </div>
@@ -103,3 +109,4 @@ export const MainComponent = ({ data }) => {
     )
 
 }
+
