4c692dcfb6c8df28ba54a10dbd96a145b6cc0cbf
FrancescoCialfiDev <cialfifrancesco02@gmail.com>
2024-12-18 19:09:13 +0100

    Added function for creating cards

--- src/components/MainComponent/MainComponent.jsx
+++ src/components/MainComponent/MainComponent.jsx
@@ -1,10 +1,15 @@
+// Importiamo lo useState di React
+import { useState } from "react"
 
 // Creiamo una funzione / componente con un return che include la logica e la struttura html della pagina.
 // Utilizziamo un export inline su una costante inizializzata ad una funzione anonima.
 export const MainComponent = ({ data }) => {
 
-    const dataCopy = [...data]
-    const newElement = dataCopy.map((element) => {
+    // Copia dell'array "data" passato come prop.
+    const [reactData, setReactData] = useState(data) // Creaimo una variabile reattiva inizializzata al valore di dataCopy
+
+    // Creiamo una costante per generare tutte le card dell0array e lavoriamo sulla variabile reattiva.
+    const newElement = reactData.map((element) => {
         return (
             element.published && (
                 <div className="col-xl-3 col-md-4 col-sm-6" key={element.id}>
@@ -22,6 +27,40 @@ export const MainComponent = ({ data }) => {
         )
     })
 
+    // State of variables
+    const [title, setTitle] = useState("")
+    const [image, setImage] = useState("")
+    const [content, setContent] = useState("")
+
+    // Submit function 
+    const submitFunction = (event) => {
+        event.preventDefault()
+        alert("The card has been generated")
+
+        const newCardModel = {
+
+            id: 1,
+            title: title,
+            image: image,
+            content: content,
+            published: true,
+
+        }
+
+        const newCard = [...reactData, newCardModel]
+        setReactData(newCard)
+    }
+
+    // Function for change Input
+    const changeTitle = (event) => {
+        setTitle(event.target.value)
+    }
+    const selectImage = (event) => {
+        setImage(event.target.value)
+    }
+    const changeContent = (event) => {
+        setContent(event.target.value)
+    }
 
 
 
@@ -30,14 +69,14 @@ export const MainComponent = ({ data }) => {
 
         <main className="d-flex" >
             <div className="formContainer">
-                <form className="p-5 bg-white rounded-3 m-4">
+                <form className="p-5 bg-white rounded-3 m-4" onSubmit={submitFunction}>
                     <div className="form-group">
                         <label htmlFor="titleForm"><span className="fw-bold">Card&apos;s Name</span></label>
-                        <input type="text" value="" className="form-control" id="titleForm" placeholder="Es: React Components" />
+                        <input type="text" value={title} className="form-control" id="titleForm" placeholder="Es: React Components" onChange={changeTitle} />
                         <label htmlFor="imageForm"><span className="fw-bold">Card&apos;s Images</span></label>
-                        <input type="file" value="" className="form-control" id="imageForm" />
+                        <input type="file" value={image} className="form-control" id="imageForm" onChange={selectImage} />
                         <label htmlFor="contentForm"><span className="fw-bold">Card&apos;s Content</span></label>
-                        <input type="text" value="" className="form-control" id="contentForm" placeholder="Es: Text of the arguments" />
+                        <input type="text" value={content} className="form-control" id="contentForm" placeholder="Es: Text of the content" onChange={changeContent} />
                     </div>
                     <button type="submit" className="btn btn-primary mt-2">Submit</button>
                 </form>
